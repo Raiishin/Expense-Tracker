1875ae3dc8d1e40c5b58c1e42072f9bb8b05d60b
Raiishin <gavin.tanjy11@gmail.com>
2022-03-15 21:30:33 +0800

    Added :: Top Bar with Buttons

---
+++ components/TopBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const topBar = {
+	height: "45%",
+	width: "100%",
+	backgroundColor: "grey",
+};
+
+const TopBar = () => {
+	const router = useRouter();
+	const redirect = async (location) => {
+		await router.push(location);
+	};
+
+	return (
+		<div style={topBar}>
+			<button onClick={(e) => redirect("/")}>Home</button>
+			<button onClick={(e) => redirect("/report")}>Report</button>
+			<button onClick={(e) => redirect("/wallets")}>Wallets</button>
+			<button onClick={(e) => redirect("/transactions")}>Transaction</button>
+			<button style={{ float: "right" }} onClick={(e) => redirect("/login")}>
+				Login
+			</button>
+		</div>
+	);
+};
+
+export default TopBar;
