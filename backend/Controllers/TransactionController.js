bc34e2a50e65d51dedfadd17f7ab5872d2cee057
Raiishin <gavin.tanjy11@gmail.com>
2022-03-28 16:58:21 +0800

    Added :: Transactions API Integration

---
+++ backend/Controllers/TransactionController.js
@@ -0,0 +1,86 @@
+// User Controller
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc, query, where } from "firebase/firestore/lite";
+import config from "../config/index.js";
+
+// Initialize Firebase
+const app = initializeApp(config.firebaseConfig);
+const db = getFirestore(app);
+const transactions = collection(db, "transactions");
+
+const index = async (req, res, next) => {
+	try {
+		const { user_id } = req.query;
+		const q = query(transactions, where("user_id", "==", user_id));
+		const transactionsSnapshot = await getDocs(q);
+		const transactionsData = transactionsSnapshot.docs.map((doc) => doc.data());
+		return res.json({ transactionsData });
+	} catch (err) {
+		next(err);
+	}
+};
+
+const view = async (req, res, next) => {
+	try {
+		const { user_id, wallet_name } = req.query;
+		const q = query(
+			transactions,
+			where("user_id", "==", user_id),
+			where("wallet_name", "==", wallet_name)
+		);
+		const transactionsSnapshot = await getDocs(q);
+		const transactionsData = transactionsSnapshot.docs.map((doc) => doc.data());
+		return res.json({ transactionsData });
+	} catch (err) {
+		next(err);
+	}
+};
+
+const create = async (req, res, next) => {
+	try {
+		const { description, date, amount, user_id, wallet_name } = req.query;
+
+		const wallets = collection(db, "wallets");
+		const walletQuery = query(
+			wallets,
+			where("user_id", "==", user_id),
+			where("name", "==", wallet_name)
+		);
+		const walletData = await getDocs(walletQuery);
+
+		console.log(walletData._docs);
+		// Error handling if there are no results
+		if (walletData._docs.length == 0) return res.json({ message: "No such wallet" });
+
+		const q = query(
+			transactions,
+			where("description", "==", description),
+			where("date", "==", date),
+			where("amount", "==", amount),
+			where("user_id", "==", user_id),
+			where("wallet_name", "==", wallet_name)
+		);
+		const search = await getDocs(q);
+
+		if (search._docs.length !== 0) return res.json({ message: "This transaction already exists" });
+
+		// Create new wallet
+		const resp = await addDoc(transactions, {
+			description: description,
+			date: date,
+			amount: amount,
+			user_id: user_id,
+			wallet_name: wallet_name,
+		});
+
+		return res.json({ id: resp.id });
+	} catch (err) {
+		next(err);
+	}
+};
+
+export default {
+	index,
+	view,
+	create,
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 import UserController from "../Controllers/UserController.js";
 import WalletController from "../Controllers/WalletController.js";
+import TransactionController from "../Controllers/TransactionController.js";
 import express from "express";
 const router = express.Router();
 
@@ -10,5 +11,9 @@ router.post("/user", UserController.create);
 router.get("/wallets", WalletController.index);
 router.get("/wallet", WalletController.view);
 router.post("/wallet", WalletController.create);
+
+router.get("/transactions", TransactionController.index);
+router.get("/transaction", TransactionController.view);
+router.post("/transaction", TransactionController.create);
 
 export default router;
